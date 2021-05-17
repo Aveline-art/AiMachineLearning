bebd9fc2f4d8af2ce8b8e369787dec9848267019
avali4907 <avali4907@outlook.com>
2021-05-17 12:58:52 -0700

    Completed hog and unittests

--- hog.py
+++ hog.py
@@ -1,12 +1,18 @@
 import random
+import unittest
+import unittests
 
 # Returns a function that acts as rolling a dice of the specified sides
 def dice(sides):
     return lambda: random.randrange(1, sides+1, step=1)
 
+d4 = dice(4)
+d6 = dice(6)
+d8 = dice(8)
+
 # Simulate rolling num of a specified dice function
-def rolls(num, dice):
-    return [dice() for val in range(num)]
+def rolls(num_dice, dice):
+    return [dice() for i in range(num_dice)]
 
 # Returns a score of 1 if any rolls are 1
 def sow_sad(results):
@@ -18,13 +24,139 @@ def sow_sad(results):
 # If number of dice rolled is 0, return squared lowest digit of opp_score + 3
 def piggy_points(opp_score):
     def helper(val, arr):
-        if val // 10 == 0:
-            return arr + [val]
+        if val < 10:
+            return [val] + arr
         else:
-            return helper(val // 10, arr + [val % 10])
+            return helper(val // 10, [val % 10] + arr)
     
     return min(helper(opp_score, [])) ** 2 + 3
 
-# Returns the new current_score
-def scoring(results, current_score, opp_score):
-    return
+# Returns true if the player can take another turn, based on more_boar
+def more_boar(player_score, opp_score):
+    def helper(val, arr):
+        if val < 10:
+            if not arr:
+                return [0] + [val]
+            return [val] + arr
+        else:
+            return helper(val // 10, [val % 10] + arr)
+    
+    player_split = helper(player_score, [])
+    opp_split = helper(opp_score, [])
+    if player_split[0] < opp_split[0] and player_split[1] < opp_split[1]:
+        return True
+    else:
+        return False
+
+# Returns true if conditions for time_trot is met, else false
+def time_trot(num_dice, turn_num, trot):
+    if num_dice == turn_num % 8 and not trot:
+        return True
+    else:
+        return False
+
+# Returns the end score for player after taking a turn, considering sow_sad and piggy_points
+def turn(num_dice, dice, player_score, opp_score):
+    # Player new score if piggy points
+    if num_dice == 0:
+        return player_score + piggy_points(opp_score)
+    results = rolls(num_dice, dice)
+
+    # Player new score if sow sad
+    is_sow_sad = sow_sad(results)
+    if is_sow_sad:
+        return player_score + is_sow_sad
+    
+    # Player score if regular
+    return player_score + sum(results)
+
+# Return 0 for player 0 or 1 for player 1 based on more_boar and time_trot
+def next_turn_player(current_player, more_boar, time_trot):
+    if more_boar:
+        return current_player
+    if time_trot:
+        return current_player
+    else:
+        return 1 - current_player
+
+def play(strat0, strat1, score0=0, score1=0, start_dice=d6, goal=100):
+    random.seed() 
+
+    # Presets  
+    current_player = 0
+    turn_num = 0
+    trot = False
+    current_dice = start_dice
+    winner = has_won(score0, score1, goal)
+
+    # While loop for the game. Mayber use a recursion somewhere?
+    while winner not in [0, 1]:
+        if current_player == 0:
+            # Pick number of dice based on strategy
+            num_dice = strat0(score0, score1, turn_num, trot)
+
+            # Find if dice chosen results in trot
+            trot = time_trot(num_dice, turn_num, trot)
+
+            # If a trot ever happens, just once, dice changes
+            if trot:
+                current_dice = d8
+            
+            # Calculate new score
+            score0 = turn(num_dice, current_dice, score0, score1)
+
+            # Determine next player based on more_boar and trot
+            next_player = next_turn_player(current_player, more_boar(score0, score1), trot)
+        else:
+            # Pick number of dice based on strategy
+            num_dice = strat1(score1, score0, turn_num, trot)
+
+            # Find if dice chosen results in trot
+            trot = time_trot(num_dice, turn_num, trot)
+
+            # If a trot ever happens, just once, dice changes
+            if trot:
+                current_dice = d8
+
+            # Calculate new score
+            score1 = turn(num_dice, current_dice, score1, score0)
+
+            # Determine next player based on more_boar and trot
+            next_player = next_turn_player(current_player, more_boar(score1, score0), trot)
+        
+        # Turn counter goes up after a player takes a turn
+        turn_num += 1
+
+        # Resets dice when player switches
+        if current_player != next_player:
+            current_dice = start_dice
+        
+        # Set next player
+        current_player = next_player
+
+        print(f"Player0 score is {score0} and Player1 score is {score1}. Next player is {current_player}")
+
+        # Determine if anyone has won
+        winner = has_won(score0, score1, goal)
+    
+    print(f"Winner is: {winner}.")
+
+# Returns the winner based on scores. None if no one wins.
+def has_won(score0, score1, goal):
+    if score0 >= goal:
+        return 0
+    elif score1 >= goal:
+        return 1
+    else:
+        return None
+
+def strat_rand(score0, score1, turn_num, trot):
+    return random.randrange(0, 10+1, step=1)
+
+def strat_six(score0, score1, turn_num, trot):
+    return 6
+
+tests = unittest.TestLoader().loadTestsFromModule(unittests)
+unittest.TextTestRunner(verbosity=2).run(tests)
+
+play(strat_rand, strat_six)
--- unittests.py
+++ unittests.py
@@ -32,7 +32,42 @@ class TestHog(unittest.TestCase):
         self.assertTrue(hog.piggy_points(3839127312983721) == 4, "3839127312983721 should equal 4")
         self.assertFalse(hog.piggy_points(38391273129830721) == 1, "38391273129830721 should not equal 1")
         self.assertTrue(hog.piggy_points(9) == 84, "9 should  equal 84")
-
+        self.assertTrue(hog.piggy_points(10) == 3, "10 should  equal 3")
+    
+    def test_more_boar(self):
+        self.assertTrue(hog.more_boar(3472, 4599), "3472 should take another turn against 4599")
+        self.assertFalse(hog.more_boar(7, 4599), "7 should not take another turn against 4599")
+        self.assertFalse(hog.more_boar(32, 33), "32 should not take another turn against 33")
+    
+    def test_time_trot(self):
+        self.assertTrue(hog.time_trot(7, 63, False), "7 dice on turn 63 should take another turn")
+        self.assertFalse(hog.time_trot(8, 56+8, False), "8 dice should never take another turn")
+        self.assertFalse(hog.time_trot(9, 56+9, False), "9 dice should never take another turn")
+        self.assertFalse(hog.time_trot(10, 56+10, False), "10 dice should never take another turn")
+        self.assertFalse(hog.time_trot(1, 9, True), "if last turn was a time trot, never take another turn")
+    
+    def test_turn(self):
+        # Adds 27 = sum([2, 2, 3, 4, 2, 3, 4, 4, 3])
+        # random.seed(33)
+        # print(sum(hog.rolls(9, hog.dice(4))))
+        # Adds 1 from results [2, 2, 3, 4, 2, 3, 4, 4, 3, 1])
+        # random.seed(33)
+        # print(sum(hog.rolls(10, hog.dice(4))))
+        random.seed(33)
+        self.assertEqual(hog.turn(9, hog.dice(4), 50, 50), 77, "turn should bring total to 77")
+        random.seed(33)
+        self.assertEqual(hog.turn(10, hog.dice(4), 50, 50), 51, "turn should bring total to 51")
+    
+    def test_next_turn_player(self):
+        self.assertTrue(hog.next_turn_player(0, False, False) == 1, "next player is 1")
+        self.assertTrue(hog.next_turn_player(0, True, False) == 0, "next player is 0")
+        self.assertTrue(hog.next_turn_player(0, False, True) == 0, "next player is 0")
+    
+    def test_has_won(self):
+        self.assertTrue(hog.has_won(0, 100, 50) == 1, "player 1 has won")
+        self.assertFalse(hog.has_won(0, 100, 101) == 1, "player 1 has not won")
+        self.assertFalse(hog.has_won(0, 100, 101) == 0, "player 0 has not won")
+        self.assertTrue(hog.has_won(50, 49, 50) == 0, "player 0 has won")
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest()
